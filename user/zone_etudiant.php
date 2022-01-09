aa67676875d159b6c86409847f8dfee816231035
franktabs <tabuguiafrank@gmail.com>
2022-01-09 22:59:23 +0100

    1er modification zone_etudiant

--- user/zone_etudiant.php
+++ user/zone_etudiant.php
@@ -1,20 +1,21 @@
 <?php
 if (isset($connect)) {
     setcookie("etudiant", serialize($etudiant));
-    var_dump($etudiant);
 }
-if(isset($_COOKIE["etudiant"])){
-    $_COOKIE["etudiant"]=$etudiant;
+if (isset($_COOKIE["etudiant"])) {
+    $_COOKIE["etudiant"] = serialize($etudiant);
 };
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
-var_dump($etudiant);
+
+$etudiant = unserialize($_COOKIE['etudiant']);
 $a = $etudiant->nom;
 $b = $etudiant->matricule;
 echo "<p> Vous êtes conecté(e) en tant que $a Matricule: $b   </p>"
 ?>
 
-<div>
-    <p>connecter</p>
+<div class="container">
+    <div class="text-center">
+        <h1 class="fst-italic">SALUT</h1>
+        <h2 class=" fst-italic fw-normal"><?php echo $a ?></h2>
+        <h2 class=" fst-italic fw-normal">MATRICULE: <?php echo $b ?></h2>
+    </div>
 </div>
