1a34bdd171546691ab3c3e7ce92b69b7d42257d2
MOTADJE Franck Donald <97166659+Francks15@users.noreply.github.com>
2022-01-21 15:05:04 +0100

    Update approveComment.php

--- notemanager/admin/approveComment.php
+++ notemanager/admin/approveComment.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION['admin'])){
     header("location:login.php");
 }
 $id = mysqli_escape_string($con,$_GET['id']);
-$query = "UPDATE comments  set status = 1  WHERE id='$id'";
+$query = "UPDATE comments  set status = 1  WHERE id='$id'";//mise a jour requete
 if(mysqli_query($con,$query)){
     mysqli_query($con,$query);
     header("location:commentaires.php");
