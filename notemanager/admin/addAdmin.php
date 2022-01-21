e65a2dcf04e45a87e40927013f179999cbaf67a6
MOTADJE Franck Donald <97166659+Francks15@users.noreply.github.com>
2022-01-21 14:43:37 +0100

    Add files via upload

---
+++ notemanager/admin/addAdmin.php
@@ -0,0 +1,35 @@
+<?php
+
+
+$errors =[];
+if(empty($_POST['name'])){
+    $errors = "Veuillez remplir le champ nom & prénom";
+}else if(empty($_POST['email'])){
+    $errors = "Veuillez remplir le champ email";
+}else if(empty($_POST['password'])){
+    $errors = "Veuillez remplir le champ mot de passe";
+}else{       
+    $email = mysqli_escape_string($con,$_POST['email']);
+    $name = mysqli_escape_string($con,$_POST['name']);
+    
+    $password = mysqli_escape_string($con,sha1($_POST['password']));
+    $created = date('Y-m-d H:s:m');
+    $added_by = $_SESSION['admin_name'];
+    
+    if(!empty($errors)){
+        echo '<div class="alert alert-danger">
+                '.$errors.'
+            </div><br>';
+    }else{
+        $quer = "INSERT INTO admins (name,email,password,created,added_by) values('$name','$email','$password','$created','$added_by')";
+        if(mysqli_query($con,$quer)){
+            echo $message = '<div class="alert alert-success">
+                       Admin ajouté avec succés 
+                  </div>';
+        }else{
+             echo '<div class="alert alert-danger">Une erreur est survenue'.mysqli_error($con).'</div>';
+        }
+    }
+}
+
+?>
