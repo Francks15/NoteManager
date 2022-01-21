1d4301baf9bb72c22aa8712577bce7c2d7d3db41
MOTADJE Franck Donald <97166659+Francks15@users.noreply.github.com>
2022-01-21 15:31:30 +0100

    Update deletePost.php

--- notemanager/admin/deletePost.php
+++ notemanager/admin/deletePost.php
@@ -3,7 +3,7 @@ include('../database/connection.php');
 if(!isset($_SESSION['admin'])){
     header("location:login.php");
 }
-$id = mysqli_escape_string($con,$_GET['id']);
+$id = mysqli_escape_string($con,$_GET['id']);//id professeur
 $query = "DELETE  FROM professeurs WHERE id='$id'";
 if(mysqli_query($con,$query)){
     mysqli_query($con,$query);
