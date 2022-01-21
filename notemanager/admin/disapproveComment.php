fc12a7c031f7dc02824abdf4040e203d9bde3900
MOTADJE Franck Donald <97166659+Francks15@users.noreply.github.com>
2022-01-21 15:34:50 +0100

    Update disapproveComment.php

--- notemanager/admin/disapproveComment.php
+++ notemanager/admin/disapproveComment.php
@@ -3,7 +3,7 @@ include('../database/connection.php');
 if(!isset($_SESSION['admin'])){
     header("location:login.php");
 }
-$id = mysqli_escape_string($con,$_GET['id']);
+$id = mysqli_escape_string($con,$_GET['id']);//id requete
 $query = "UPDATE comments  set status = 0  WHERE id='$id'";
 if(mysqli_query($con,$query)){
     mysqli_query($con,$query);
