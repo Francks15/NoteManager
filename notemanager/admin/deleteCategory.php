788f7255ac454b3c44d088d351e6f8e0e37c4ea1
MOTADJE Franck Donald <97166659+Francks15@users.noreply.github.com>
2022-01-21 15:06:42 +0100

    Update deleteCategory.php

--- notemanager/admin/deleteCategory.php
+++ notemanager/admin/deleteCategory.php
@@ -3,7 +3,7 @@ include('../database/connection.php');
 if(!isset($_SESSION['admin'])){
     header("location:login.php");
 }
-$id = mysqli_escape_string($con,$_GET['id']);
+$id = mysqli_escape_string($con,$_GET['id']);//suppression
 $query = "DELETE  FROM module WHERE id='$id'";
 if(mysqli_query($con,$query)){
     mysqli_query($con,$query);
