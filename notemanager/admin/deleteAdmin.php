49cc03ab75a4661c0429dc940487ab7c5020a804
MOTADJE Franck Donald <97166659+Francks15@users.noreply.github.com>
2022-01-21 15:02:36 +0100

    Update deleteAdmin.php

--- notemanager/admin/deleteAdmin.php
+++ notemanager/admin/deleteAdmin.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION['admin'])){
     header("location:login.php");
 }
 $id = mysqli_escape_string($con,$_GET['id']);
-$query = "DELETE  FROM admins WHERE id='$id'";
+$query = "DELETE  FROM admins WHERE id='$id'";//requete suppression
 if(mysqli_query($con,$query)){
     mysqli_query($con,$query);
     header("location:admins.php?deleted");
